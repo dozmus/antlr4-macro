d8a36b9aea3b69b489a4f90dd4e4e4033db3c3d7
PureCS <purecs@hotmail.com>
2016-07-22 20:32:13 +0100

    Update filehelper tests

--- src/test/java/notpure/antlr4/macro/processor/FileHelperTest.java
+++ src/test/java/notpure/antlr4/macro/processor/FileHelperTest.java
@@ -2,12 +2,15 @@ package notpure.antlr4.macro.processor;
 
 import notpure.antlr4.macro.processor.util.FileHelper;
 import org.junit.Test;
+
+import java.util.Scanner;
+
 import static org.junit.Assert.assertEquals;
 
 /**
  * Tests for {@link FileHelper}.
  */
-public final class FileHelperTests {
+public final class FileHelperTest {
 
     /**
      * Tests {@link FileHelper#parseFileName(String)}.
@@ -19,6 +22,20 @@ public final class FileHelperTests {
         assertEquals("C:/ANTLR4/antlr4.macro", FileHelper.parseFileName("C:/ANTLR4/antlr4.macro.jar"));
         assertEquals("/ANTLR4/antlr4.macro", FileHelper.parseFileName("/ANTLR4/antlr4.macro.jar"));
         assertEquals("antlr4macro", FileHelper.parseFileName("antlr4macro"));
+    }
+
+    /**
+     * Tests {@link FileHelper#stringStream(String)}.
+     */
+    @Test
+    public void testStringStream() {
+        Scanner in = new Scanner(FileHelper.stringStream("hello100world"));
+        assertEquals("hello100world", in.next());
+
+        in = new Scanner(FileHelper.stringStream("hello100world;!!093"));
+        assertEquals("hello100world;!!093", in.next());
+
+        // TODO test all symbols which appear in TokenDefinition
     }
 
     @Test(expected = IllegalArgumentException.class)
